bf4bf406a86df853068a67cb684ee19190e1b986
marcelcm123 <liongmarcel@gmail.com>
2021-09-08 11:14:46 +0800

    init 1

---
+++ app/src/main/java/ac/id/ubaya/advweek4/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package ac.id.ubaya.advweek4.view
+
+import ac.id.ubaya.advweek4.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
